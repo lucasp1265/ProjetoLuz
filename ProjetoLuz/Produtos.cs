0bf262f551fcb0f5ddcde25ce3cc1f4c27d448b6
Lucas Prado <lucas.prado_15@hotmail.com>
2022-03-02 00:16:44 -0300

    .

--- ProjetoLuz/Produtos.cs
+++ ProjetoLuz/Produtos.cs
@@ -10,21 +10,7 @@ namespace ProjetoLuz
     //Classe abstrata para ser "especializada" nos outros produtos
      public abstract class Produtos
     {
-        public struct PrecoEItem
-        {
-            public int[] Preco = new int[4];
-            public string[] Item = new string[4];
-
-            public PrecoEItem(string[] produtos, int[] preco)
-            {
-                Item[0] = "Nenhum Item";
-                Preco[0] = 0;
-                for(int i = 1; i < 4; i++)
-                {
-                    Item[i] = produtos[i];
-                    Preco[i] = preco[i];
-                }
-            }
+       
 
         public int PrecoCarrinho;
 
@@ -35,15 +21,6 @@ namespace ProjetoLuz
         }
         abstract public void CalculaPreco(int indice, int quantidade);
 
-        public void CalculaPreco(int quantidade, string item)
-        {
-            int count = 0;
-            while (item != Itens.Item[count])
-            {
-                count++;
-            }
-            preco += (Itens.Preco[count]*quantidade);
-        }
 
     }
 }
