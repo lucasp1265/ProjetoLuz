d63f02fc984a85d058d69de563f56afa9ba68718
Lucas Prado <85458944+lucasp1265@users.noreply.github.com>
2022-03-03 16:12:46 -0300

    Add files via upload

---
+++ ProjetoLuz/Frutas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLuz
+{
+    public class Frutas : Produtos
+    {
+        public string[] frutas = { "Nenhum Item", "Abacaxi", "Maçã", "Melancia" };
+        public int[] precos = { 0, 22, 7, 5 };
+
+        public Frutas()
+        {
+
+        }
+        public override void CalculaPreco(int quantidade, int indice)
+        {
+
+            PrecoCarrinho = PrecoCarrinho + (precos[indice] * quantidade);
+
+        }
+    }
+}
---
+++ ProjetoLuz/Limpeza.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLuz
+{
+    public class Limpeza : Produtos
+    {
+        public string[] limpezas = { "Nenhum Item", "Desinfetante", "Água Sanitária", "Esponjas" };
+        public int[] precos = { 15, 25, 6 };
+
+        public Limpeza()
+        {
+        }
+
+        public int Desconto(int quantidade)
+        {
+            if (quantidade >= 3)
+            {
+                return (int)(PrecoCarrinho * 0.8);
+            }
+            else
+            {
+                return PrecoCarrinho;
+            }
+        }
+
+        public override void CalculaPreco(int quantidade, int indice)
+        {
+
+            PrecoCarrinho += (int)(precos[indice] * quantidade * 0.8);
+            PrecoCarrinho = Desconto(quantidade);
+
+        }
+    }
+}
